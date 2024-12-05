d3e5ceb268b79b86cd2a3c73f733b7f52cc2b289
Taxys <dvttsf@gmail.com>
2024-12-05 06:58:14 +0100

    Ahora si, projecto final

--- Practica2/app/src/main/java/dev/davidvega/practica2/GalleryAdapter.kt
+++ Practica2/app/src/main/java/dev/davidvega/practica2/GalleryAdapter.kt
@@ -10,41 +10,51 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import dev.davveg.practica2.model.GalleryCard
 
+class GalleryAdapter(
+    val galleryCardList: List<GalleryCard>,
+    private val context: Context,
+    private val itemClickListener: (GalleryCard) -> Unit  // Para implementar el click
+) : RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
 
-class GalleryAdapter : RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
-
-    var galleryCardList: List<GalleryCard>  = ArrayList()
-    lateinit var context: Context
-
-    fun RecyclerAdapter(superheros : List<GalleryCard>, context: Context){
-        this.galleryCardList = superheros
-        this.context = context
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = galleryCardList.get(position)
-        holder.bind(item, context)
-
-    }
+    private var position = 0
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return ViewHolder(layoutInflater.inflate(R.layout.item_gallery_list, parent, false))
     }
 
-    override fun getItemCount(): Int {
-        return galleryCardList.size
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = galleryCardList[position]
+        holder.bind(item, context)
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val cardImage = view.findViewById(R.id.imageGalleryItem) as ImageView
-        val cardText = view.findViewById(R.id.textGalleryItem) as TextView
-
-        fun bind(card: GalleryCard, context: Context){
-            cardImage.setImageResource(card.image)
-            cardText.setText(card.name)
-
+        holder.itemView.setOnClickListener {
+            itemClickListener(item)
         }
 
+        holder.itemView.setOnLongClickListener {
+            setPosition(holder.layoutPosition)
+            false
+        }
     }
+
+    fun getPosition(): Int {
+        return position
+    }
+    fun setPosition(position: Int) {
+        this.position = position
+    }
+
+    override fun getItemCount(): Int = galleryCardList.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val cardImage: ImageView = view.findViewById(R.id.imageGalleryItem)
+        private val cardText: TextView = view.findViewById(R.id.textGalleryItem)
+
+        fun bind(card: GalleryCard, context: Context) {
+            cardImage.setImageResource(card.image)
+            cardText.text = card.name
+        }
+    }
+
+
 }
--- Practica2/app/src/main/java/dev/davidvega/practica2/MainActivity.kt
+++ Practica2/app/src/main/java/dev/davidvega/practica2/MainActivity.kt
@@ -3,8 +3,10 @@ package dev.davidvega.practica2
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import dev.davidvega.practica2.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +16,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        enableEdgeToEdge()
+
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
 
         val adapter = FragmentAdapter(this)
         binding.pager.adapter = adapter
@@ -23,6 +29,21 @@ class MainActivity : AppCompatActivity() {
                 tab, position -> tab.text = "TAB ${(position + 1)}"
         }.attach()
 
+        binding.pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                binding.toolbar.menu.clear()
+                binding.toolbar.title = "Practica3"
+            }
+        })
+
+
 
     }
+
+
 }
